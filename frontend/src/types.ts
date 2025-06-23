781c85d26301d3de7319317cd432deadbfa035d8
Archit Mahule <archit.mahule@mathongo.com>
2025-06-24 00:04:16 +0530

    add Axios client and TypeScript interfaces

---
+++ frontend/src/types.ts
@@ -0,0 +1,26 @@
+export type SplitType = "equal" | "percentage";
+
+export interface Split {
+  user_id: number;
+  share: number;
+}
+
+export interface ExpenseCreate {
+  description: string;
+  amount: number;
+  paid_by: number;
+  split_type: SplitType;
+  splits?: Split[];
+}
+
+export interface GroupCreate {
+  name: string;
+  user_ids: number[];
+}
+
+export interface Group {
+  id: number;
+  name: string;
+  user_ids: number[];
+  total_expenses: number;
+}
