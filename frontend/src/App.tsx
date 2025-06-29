11b03678e98b8e126d060d178f0b4d383fae130b
Archit Mahule <archit.mahule@mathongo.com>
2025-06-29 15:39:47 +0530

    refactor: simplify routing structure by integrating AppShell component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,21 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import CreateGroup from "./components/CreateGroup";
-import AddExpense from "./components/AddExpense";
-import GroupBalances from "./components/GroupBalances";
-import UserBalances from "./components/UserBalances";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppShell } from './components/layout/AppShell';
+import CreateGroup from './components/CreateGroup';
+import AddExpense from './components/AddExpense';
+import GroupBalances from './components/GroupBalances';
+import UserBalances from './components/UserBalances';
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="bg-blue-600 text-white p-4">
-        <ul className="flex space-x-4">
-          <li><Link to="/">Create Group</Link></li>
-          <li><Link to="/add-expense">Add Expense</Link></li>
-          <li><Link to="/group-balances">Group Balances</Link></li>
-          <li><Link to="/user-balances">User Balances</Link></li>
-        </ul>
-      </nav>
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<CreateGroup />} />
-          <Route path="/add-expense" element={<AddExpense />} />
-          <Route path="/group-balances" element={<GroupBalances />} />
-          <Route path="/user-balances" element={<UserBalances />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route element={<AppShell/>}>
+          <Route index element={<CreateGroup />} />
+          <Route path="add-expense" element={<AddExpense />} />
+          <Route path="group-balances" element={<GroupBalances />} />
+          <Route path="user-balances" element={<UserBalances />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
